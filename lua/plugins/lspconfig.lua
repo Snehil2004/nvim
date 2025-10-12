cd21cff4eda95b90864ca1e92c1e4639fe4d3ce0
snehil <snehilsuthar81@gmail.com>
2025-10-12 13:37:10 +0530

    reconfigured lsp.lua file

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,115 +1,160 @@
--- Updated for Neovim 0.11+ with proper vim.lsp.config usage and mason integration
-
+-- Quick Fix: Hybrid approach that ensures TypeScript works
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+--[[  	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+		config = function()
+			-- Enhanced capabilities
+			local capabilities = vim.tbl_deep_extend(
+				"force",
+				vim.lsp.protocol.make_client_capabilities(),
+				require("cmp_nvim_lsp").default_capabilities()
+			)
 
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "neovim/nvim-lspconfig" },
-    config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend(
-        "force",
-        capabilities,
-        require("cmp_nvim_lsp").default_capabilities()
-      )
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-      capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+			capabilities.textDocument.completion.completionItem.snippetSupport = true
+			capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
-      vim.diagnostic.config({
-        virtual_text = false,
-        signs = true,
-        underline = true,
-        update_in_insert = false,
-      })
+			-- Modern diagnostic configuration
+			vim.diagnostic.config({
+				virtual_text = false,
+				signs = true,
+				underline = true,
+				update_in_insert = false,
+				severity_sort = true,
+			})
 
-      -- Register all configurations before attaching
-      vim.lsp.config("clangd", {
-        on_attach = function(client, bufnr)
-          -- client.server_capabilities.documentFormattingProvider = false
-          -- client.server_capabilities.documentRangeFormattingProvider = false
-        end,
-      })
+			-- Common on_attach function
+			local on_attach = function(client, bufnr)
+				-- Modern LSP keymaps
+				local opts = { buffer = bufnr, silent = true }
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+		end
 
-      vim.lsp.config("html", { capabilities = capabilities })
-      vim.lsp.config("cssls", { capabilities = capabilities })
-      vim.lsp.config("jsonls", { capabilities = capabilities })
-      vim.lsp.config("marksman", { capabilities = capabilities })
+			-- Install servers via Mason
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"ts_ls",
+					"eslint",
+					"html",
+					"cssls",
+					"jsonls",
+					"lua_ls",
+					"pyright",
+					"ruff",
+					"clangd",
+					"marksman",
+					"emmet_language_server",
+				},
+			})
 
-      vim.lsp.config("pyright", {
-        capabilities = capabilities,
-        settings = {
-          pyright = { disableOrganizeImports = true },
-          python = { analysis = { ignore = { "*" } } },
-        },
-      })
+			-- DIRECT lspconfig setup (bypassing mason-lspconfig handlers to avoid conflicts)
 
-      vim.lsp.config("ruff", {
-        capabilities = capabilities,
-        filetypes = { "python" },
-        on_attach = function(client, bufnr)
-          client.server_capabilities.hoverProvider = false
-        end,
-      })
+			-- TypeScript - DIRECT SETUP
+			require("lspconfig").ts_ls.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					on_attach(client, bufnr)
+				end,
+				settings = {
+					typescript = {
+						inlayHints = {
+							includeInlayParameterNameHints = "all",
+							includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+							includeInlayFunctionParameterTypeHints = true,
+							includeInlayVariableTypeHints = true,
+						},
+					},
+				},
+			})
 
-      vim.lsp.config("emmet_language_server", {
-        filetypes = {
-          "html", "css", "scss", "javascriptreact", "typescriptreact",
-          "vue", "svelte", "xml", "pug"
-        },
-        init_options = {
-          html = {
-            options = {
-              ["bem.enabled"] = true,
-            },
-          },
-        },
-      })
+			-- ESLint
+			require("lspconfig").eslint.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					on_attach(client, bufnr)
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						command = "EslintFixAll",
+					})
+				end,
+			})
 
-      vim.lsp.config("eslint", {
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          -- You can add eslint-specific on_attach logic here
-        end,
-      })
+			-- Lua
+			require("lspconfig").lua_ls.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					Lua = {
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+					},
+				},
+			})
 
-      vim.lsp.config("ts_ls", {
-        capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          -- Disable semantic tokens if needed
-          -- client.server_capabilities.semanticTokensProvider = nil
-        end,
-        flags = {
-          debounce_text_changes = 150,
-        },
-      })
+			-- Python
+			require("lspconfig").pyright.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				settings = {
+					pyright = { disableOrganizeImports = true },
+					python = { analysis = { ignore = { "*" } } },
+				},
+			})
 
-      vim.lsp.config("lua_ls", {
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { "vim" },
-            },
-          },
-        },
-      })
+			require("lspconfig").ruff.setup({
+				capabilities = capabilities,
+				on_attach = function(client, bufnr)
+					on_attach(client, bufnr)
+					client.server_capabilities.hoverProvider = false
+				end,
+			})
 
-      -- Setup mason-lspconfig to apply handlers for installed servers
-      require("mason-lspconfig").setup({
-        ensure_installed = {},
-        handlers = {
-          function(server_name)
-            require("lspconfig")[server_name].setup({})
-          end,
-        },
-      })
-    end,
-  },
+			-- C/C++
+			require("lspconfig").clangd.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+			})
+
+			-- Web Development
+			require("lspconfig").html.setup({ capabilities = capabilities, on_attach = on_attach })
+			require("lspconfig").cssls.setup({ capabilities = capabilities, on_attach = on_attach })
+			require("lspconfig").jsonls.setup({ capabilities = capabilities, on_attach = on_attach })
+
+			require("lspconfig").emmet_language_server.setup({
+				capabilities = capabilities,
+				on_attach = on_attach,
+				filetypes = {
+					"html",
+					"css",
+					"scss",
+					"javascriptreact",
+					"typescriptreact",
+					"vue",
+					"svelte",
+				},
+			})
+
+			-- Markdown
+			require("lspconfig").marksman.setup({ capabilities = capabilities, on_attach = on_attach })
+
+end,
+	}, ]]
 }
-
